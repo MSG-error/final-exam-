d1629f1fe1168a2ec23ff986afe57a66f1bbae8a
MSG-error <k33lh187@gmail.com>
2024-12-26 14:57:53 +0300

    Final

---
+++ Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+
+public class Vehicle
+{
+    public string Name { get; set; }
+    private int speed;
+    public static int TotalVehicles;
+
+    public Vehicle(string name, int startingSpeed)
+    {
+        Name = name;
+        speed = startingSpeed;
+        TotalVehicles++;
+    }
+
+    public virtual void Start() => Console.WriteLine("Vehicle is starting!");
+    public void Accelerate(int increment) => speed += increment;
+    public int GetSpeed() => speed;
+}
+
+public class Car : Vehicle
+{
+    public Car(string name, int startingSpeed) : base(name, startingSpeed) { }
+    public override void Start() => Console.WriteLine("The car is starting with a smooth engine sound!");
+}
+
+public class Motorcycle : Vehicle
+{
+    public Motorcycle(string name, int startingSpeed) : base(name, startingSpeed) { }
+    public override void Start() => Console.WriteLine("The motorcycle roars to life!");
+   
+}
+
+public class Truck : Vehicle
+{
+    public Truck(string name, int startingSpeed) : base(name, startingSpeed) { }
+    public override void Start() => Console.WriteLine("The truck's heavy engine rumbles as it starts!");
+}
+
+class Program
+{
+    static void Main()
+    {
+        Vehicle[] vehicles = {
+            new Car("Car", 10),
+            new Motorcycle("Motorcycle", 20),
+            new Truck("Truck", 15)
+        };
+
+        foreach (var vehicle in vehicles)
+            vehicle.Start();
+
+        vehicles[0].Accelerate(10); // Car
+        vehicles[1].Accelerate(20); // Motorcycle
+        vehicles[2].Accelerate(15); // Truck
+
+        foreach (var vehicle in vehicles)
+            Console.WriteLine($"{vehicle.Name} speed: {vehicle.GetSpeed()} mph");
+
+        Console.WriteLine($"Total vehicles created: {Vehicle.TotalVehicles}");
+    }
+}
+
